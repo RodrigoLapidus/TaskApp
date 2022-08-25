d8d6b558014ec96c7cb24cf5ec4fcd815d4da997
Rodrigo Lopez <rodrigo.lapidus@gmail.com>
2022-08-25 15:41:37 +0200

    update of the live site

--- src/components/SignOut.vue
+++ src/components/SignOut.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <button @click="signOut">Sign Out</button>
+        <button @click="signOut" class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 rounded">Sign Out</button>
     </div>
 </template>
 
